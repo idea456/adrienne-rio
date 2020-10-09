59b1f0fa4baaa1359e95a1e26a0bdefe108d3d35
Adrienne Rio Wongso Atmojo <riowongsoatmojo@gmail.com>
2020-10-09 20:57:12 +0800

    Updates

---
+++ precache-manifest.c231e0d5269fef7aa0746dc76348c669.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64803d9cbf56d77899edda595df3e95b",
+    "url": "/adrienne-rio/index.html"
+  },
+  {
+    "revision": "976bfaac7444c339e724",
+    "url": "/adrienne-rio/static/css/2.aabc9aba.chunk.css"
+  },
+  {
+    "revision": "9f03c3df42415bd780a0",
+    "url": "/adrienne-rio/static/css/main.6e40b20c.chunk.css"
+  },
+  {
+    "revision": "976bfaac7444c339e724",
+    "url": "/adrienne-rio/static/js/2.588bfda5.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/adrienne-rio/static/js/2.588bfda5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f03c3df42415bd780a0",
+    "url": "/adrienne-rio/static/js/main.c5a9239b.chunk.js"
+  },
+  {
+    "revision": "94fd4a100af28e1e433c",
+    "url": "/adrienne-rio/static/js/runtime-main.4bab29d3.js"
+  },
+  {
+    "revision": "2aaf931ee67608f54be21b06a994f485",
+    "url": "/adrienne-rio/static/media/TTCommons-Bold.2aaf931e.ttf"
+  },
+  {
+    "revision": "a7d226cc9d9a7936e3b57ab9f303b8ca",
+    "url": "/adrienne-rio/static/media/TTCommons-Medium.a7d226cc.ttf"
+  },
+  {
+    "revision": "64f3f998447493fc2bf1019fd0bebdc2",
+    "url": "/adrienne-rio/static/media/TTCommons-Regular.64f3f998.ttf"
+  },
+  {
+    "revision": "1859a3564442607f05e5b858e085f5bf",
+    "url": "/adrienne-rio/static/media/cv.1859a356.pdf"
+  },
+  {
+    "revision": "1ea82e3afe161b05826fed375f43f0ca",
+    "url": "/adrienne-rio/static/media/profile.1ea82e3a.jpg"
+  }
+]);
