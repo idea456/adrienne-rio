022bdbf9d02ecedb697a7334377e1bc252746a1b
Adrienne Rio Wongso Atmojo <riowongsoatmojo@gmail.com>
2020-10-10 19:14:19 +0800

    fixed carousel alignment

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,7 @@ import { description } from "../constants";
 
 export default function Skills(props) {
   return (
-    <div style={{ marginRight: "1.7vw" }}>
+    <div style={{ fontSize: "0.9em" }}>
       <h1>{props.title}</h1>
       {props.skills.map((skill) => (
         <Tooltip placement="top" title={description[skill]} enterDelay={130}>
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -4,6 +4,7 @@ import ProjectCard from "../components/ProjectCard";
 import Button from "@material-ui/core/Button";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import Grid from "@material-ui/core/Grid";
 import {
   CarouselProvider,
   Slider,
@@ -19,6 +20,7 @@ import "../css/Slider.css";
 import "../css/ProjectList.css";
 import "glider-js/glider.min.css";
 import { dict, projects } from "../constants";
+import { GridListTile } from "@material-ui/core";
 
 export default function ProjectPage(props) {
   const [list, setList] = useState(projects);
@@ -56,8 +58,6 @@ export default function ProjectPage(props) {
     <div
       style={{
         display: "flex",
-        marginLeft: 90,
-        marginRight: 90,
         marginTop: 100,
         marginBottom: 60,
         justifyContent: "center",
@@ -71,7 +71,7 @@ export default function ProjectPage(props) {
           width: 250,
           fontSize: 75,
           margin: 0,
-          marginLeft: 12,
+          marginLeft: 102,
           // borderBottom: "8px solid #faed27",
         }}
       >
@@ -81,7 +81,8 @@ export default function ProjectPage(props) {
       <div
         style={{
           marginBottom: 15,
-          marginLeft: 12,
+          marginLeft: 102,
+          marginRight: 90,
           display: "flex",
           flexWrap: "wrap",
         }}
@@ -157,89 +158,81 @@ export default function ProjectPage(props) {
         })}
       </div>
 
-      <div>
-        {list.length === 0 && (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "row",
-              fontSize: 20,
-              height: 0.48 * window.innerHeight,
-            }}
-          >
-            <h1>No projects found 🤷‍♂️</h1>
-          </div>
-        )}
-        {list.length !== 0 && (
-          <CarouselProvider
-            naturalSlideWidth={400}
-            naturalSlideHeight={0.3 * window.innerHeight}
-            totalSlides={list.length}
-            visibleSlides={2}
-            isIntrinsicHeight
-          >
-            <div
-              style={{
-                position: "absolute",
-                top: "428%",
-                left: "3%",
-              }}
-            >
+      {list.length === 0 && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "row",
+            fontSize: 20,
+            height: 0.48 * window.innerHeight,
+          }}
+        >
+          <h1>No projects found 🤷‍♂️</h1>
+        </div>
+      )}
+      {list.length !== 0 && (
+        <CarouselProvider
+          naturalSlideWidth={400}
+          naturalSlideHeight={0.3 * window.innerHeight}
+          totalSlides={list.length}
+          visibleSlides={2}
+          isIntrinsicHeight
+        >
+          <Grid container alignItems="center" justifyContent="center">
+            <Grid xs={1} sm={1} md={1} lg={1} xl={1}>
               <ButtonBack
-                style={{ backgroundColor: "transparent", border: "none" }}
+                style={{
+                  backgroundColor: "transparent",
+                  border: "none",
+                  marginLeft: "4vw",
+                }}
               >
                 <ArrowBackIosIcon style={{ fontSize: 40 }} />
               </ButtonBack>
-            </div>
+            </Grid>
+            <Grid xs={10} sm={10} md={10} lg={10} xl={10}>
+              <Slider>
+                {list.map((project, i) => {
+                  return (
+                    <Slide index={i}>
+                      <ProjectCard
+                        key={i}
+                        style="projectCard"
+                        title={project.title}
+                        description={project.description}
+                        techStack={project.techStack}
+                        githubLink={project.githubLink}
+                      />
+                    </Slide>
+                  );
+                })}
+              </Slider>
+            </Grid>
 
-            <Slider>
-              {list.map((project, i) => {
-                return (
-                  <Slide index={i}>
-                    <ProjectCard
-                      key={i}
-                      style="projectCard"
-                      title={project.title}
-                      description={project.description}
-                      techStack={project.techStack}
-                      githubLink={project.githubLink}
-                    />
-                  </Slide>
-                );
-              })}
-            </Slider>
-
-            <div
-              style={{
-                position: "absolute",
-                top: "428%",
-                right: "3%",
-              }}
-            >
+            <Grid xs={1} sm={1} md={1} lg={1} xl={1}>
               <ButtonNext
                 style={{ backgroundColor: "transparent", border: "none" }}
               >
                 <ArrowForwardIosIcon style={{ fontSize: 40 }} />
               </ButtonNext>
-            </div>
-
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: 20,
-              }}
-            >
-              {list.map((p, i) => {
-                if (i % 2 === 0) {
-                  return <Dot slide={i} />;
-                }
-              })}
-            </div>
-          </CarouselProvider>
-        )}
-      </div>
+            </Grid>
+          </Grid>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: 20,
+            }}
+          >
+            {list.map((p, i) => {
+              if (i % 2 === 0) {
+                return <Dot slide={i} />;
+              }
+            })}
+          </div>
+        </CarouselProvider>
+      )}
     </div>
   );
 }
